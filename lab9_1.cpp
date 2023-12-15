b5e5776a7cc0188b0a03464d4418878a5bf5d731
Best <Photchara_k@cmu.ac.th>
2023-12-15 16:38:44 +0700

    คอมพีท

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,33 +1,47 @@
-#include<iostream>
-#include<iomanip> //For using setw(), setprecision(), ...
+#include <iostream>
+#include <iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
-	cout << "Enter initial loan: ";
-	cout << "Enter interest rate per year (%): ";
-	cout << "Enter amount you can pay per year: ";
+int main()
+{
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
-	cout << setw(13) << left << "EndOfYear#"; 
-	cout << setw(13) << left << "PrevBalance"; 
-	cout << setw(13) << left << "Interest"; 
+	double money, tax, have, pasi, total;
+	cout << "Enter initial loan: ";
+	cin >> money;
+	cout << "Enter interest rate per year (%): ";
+	cin >> pasi;
+	cout << "Enter amount you can pay per year: ";
+	cin >> have;
+
+	cout << setw(13) << left << "EndOfYear#";
+	cout << setw(13) << left << "PrevBalance";
+	cout << setw(13) << left << "Interest";
 	cout << setw(13) << left << "Total";
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+
+	int i = 1;
+	while (money>0)
+	{
+		cout << fixed << setprecision(2);
+		cout << setw(13) << left << i;
+		cout << setw(13) << left << money;
+		tax = (money * pasi) / 100;
+		cout << setw(13) << left << tax;
+		total = money + tax;
+		cout << setw(13) << left << total;
+		if (total < have)
+		{
+			have = total;
+		}
+		cout << setw(13) << left << have;
+		money = total - have;
+
+		cout << setw(13) << left << money;
+		cout << "\n";
+
+		i++;
+	}
 	return 0;
 }
--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,8 +1,20 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int x ,int y){
 
+if(x<=0 or y<=0){
+cout << "Invalid input";
+}else{
+        for(int i = 0;i<x;i++){
+            for(int j = 0; j<y;j++){
+                cout << "O";
+                }
+                cout << "\n" ;
+        }
+    }
+}
+    
 int main(){
 	
 	printO(2,2);
